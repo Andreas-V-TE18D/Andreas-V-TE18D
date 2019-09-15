6930071c9ac6026f0ff82b67356a33a0e343d958
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2019-09-15 23:31:35 +0200

    Update hemläxav37.java
    har gjort läxan

--- src/hemläxav37.java
+++ src/hemläxav37.java
@@ -1,9 +1,44 @@
+import java.util.Scanner;
 
 public class hemläxav37 {
-
+	//1. Take input for strings 
+	//2. Take input for integers 
+	//3. Take input for doubles 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		//step 1: Declare each variable 
+		String name1 = "Andreas";
+		int age = 0;
+		String name2 = "nothing"; 
+		int postnummer = 0;
+		String name3 = "nothing";
+		int userPhonenumber = 0;
+		
+		//Step 2: Prompt user and take input 
+		System.out.print("Vad heter du: ");
+		name1 = s.next();
+		System.out.print("Hur gammal är du: ");
+		age = s.nextInt();
+		System.out.print("Vad är din adress: ");
+		name2 = s.next();
+		System.out.print("Vad är ditt postnummer: ");
+		postnummer = s.nextInt();
+		System.out.print("Vilken stad bor du i: ");
+		name3 = s.next();
+		System.out.print("Vad är ditt telefon nummer: ");
+		userPhonenumber = s.nextInt();
+		
+		//Step 3: Use inputs.
+		System.out.println("Information");
+		System.out.println("Namn:  "+name1);
+		System.out.println("Ålder: "+age+" år");
+		System.out.println("Adress: "+name2+",  "+postnummer+",  "+name3+", ");
+		System.out.println("Ditt telefon nummer är: "+userPhonenumber);
+				
+		
+		
 	}
-lw3ktnl
+
 }
