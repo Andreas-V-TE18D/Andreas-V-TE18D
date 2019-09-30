cc4adbbf5a4c4ad6fd5226566c66619e029522c1
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2019-09-30 11:25:12 +0200

    gjort klart två läxor

--- Homework/Quadrant.java
+++ Homework/Quadrant.java
@@ -1,9 +1,15 @@
+import java.util.*;
 
 public class Quadrant {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(solve(x, y));
+    }
+    public static int solve(int x, int y){
+        if(x < 0)
+            return (y < 0)? 3 : 2;
+        return (y < 0)? 4 : 1;
+    }
 }
---
+++ Homework/stuckinatimeloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class stuckinatimeloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        for(int i = 1; i < cases+1; i++)
+            System.out.println(i + " Abracadabra");
+    }
+}
