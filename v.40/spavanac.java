58b0c6d7b1e187ca4635503d2a9979ebd6f4a9f2
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2019-10-07 23:23:15 +0200

    gjort klart andra läxan 4, kolla description
    Förkortat och gjort klart läxan spavanac och samt kommentarer.

--- v.40/spavanac.java
+++ v.40/spavanac.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
 public class spavanac {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+    Scanner input = new Scanner(System.in); // initializes the scanner 
+    
+    int hours = input.nextInt(); // puts answer in varible
+    int minutes = input.nextInt();
+    
+    minutes -= 45;
+    
+    if (minutes < 0)
+    {
+    	hours -= 1;
+    	minutes += 60;
+    }
+    if (hours < 0)
+    	hours = 23;
+    
+    System.out.println(hours + " " + minutes); // prints the answer 
+    
+    
+    input.close(); // gets rid off anoying yellow sign:) 
+    
 	}
 
 }
